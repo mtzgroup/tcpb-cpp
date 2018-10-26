fd70c8d652833b971c28bc841052ba09ff7d4b4f
Stefan Seritan <sseritan@gmail.com>
2018-10-26 15:51:33 -0700

    Another attempt to fix the testing framework.

--- tests/mock_server.py
+++ tests/mock_server.py
@@ -12,8 +12,6 @@ import struct
 from threading import Thread
 from Queue import Queue
 
-from tcpb import terachem_server_pb2 as pb
-
 
 class MockServer(object):
     """Mock server for TCPB client testing
