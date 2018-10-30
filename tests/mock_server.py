7026302989b99711158a1d92407d94f9ec9a06f3
Stefan Seritan <sseritan@gmail.com>
2018-10-29 18:33:38 -0700

    Create Python protobuf output for mock server in tests

--- tests/mock_server.py
+++ tests/mock_server.py
@@ -12,7 +12,8 @@ import struct
 from threading import Thread
 from Queue import Queue
 
-from tcpb import terachem_server_pb2 as pb
+# Must be created by protoc (done in BitBucket Pipelines)
+from . import terachem_server_pb2 as pb
 
 
 class MockServer(object):
