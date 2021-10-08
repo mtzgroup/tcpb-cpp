547603b49190ae8df5ac8dc18052efaa55b0209b
Vinicius Wilian D. Cruzeiro <vwcruz@stanford.edu>
2021-10-07 19:53:34 -0700

    Implement on api.cpp the tc_connect and tc_finalize functions.

--- src/api.cpp
+++ src/api.cpp
@@ -21,6 +21,35 @@ using std::vector;
 
 extern "C" {
 
-  bool NewCondition;
+  TCPB::Client* TC = nullptr;
 
-}
+  /**
+   * \brief Connects to TeraChem server
+   *
+   * @param[in] host Address of the host
+   * @param[in] port Port number
+   * @param[out] status Status of execution: 0, all is good; 1, could not connect to server; 2, connected to server but it is not available
+   **/
+  void tc_connect_(const char host[80], const int* port, int* status) {
+    try {
+      TC = new TCPB::Client(std::string(host), (*port));
+    }
+    catch (...) {
+      (*status) = 1;
+    }
+    bool avail = TC->IsAvailable();
+    if (!avail)
+      (*status) = 2;
+    else
+      (*status) = 0;
+  }
+
+  /**
+   * \brief Deletes from memory variables that are open
+   **/
+  void tc_finalize_() {
+    if (TC != nullptr)
+      delete TC;
+  }
+
+} // extern "C"
